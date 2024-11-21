6bd1b2062b21420996262be5d85f2db8820a78a8
Pablo Navarro <pablo1n7@gmail.com>
2024-11-20 22:44:44 -0300

    Add files via upload

---
+++ festival/main.py
@@ -0,0 +1,126 @@
+from flask import Flask, jsonify, render_template
+from flask_socketio import SocketIO, emit
+import json
+from datetime import datetime
+
+from flask_cors import CORS
+from OpenSSL import SSL
+import os
+import pandas as pd
+from datetime import datetime
+
+# context = SSL.Context(SSL.P)
+# context.use_privatekey_file('key.pem')
+# context.use_certificate_file('cert.pem')
+
+CANT_HITOS_USER = 10
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+CORS(app)  # Esto permite CORS para todas las rutas
+
+# Datos iniciales
+data_initial = [
+    {'user':0,"anio": 1970,'hito':0 ,"mes": 5, "dia":1, "name": "Centro Nacional Patagónico (CENPAT) comenzó a funcionar"},    
+    {'user':0,"anio":1972, "hito":1, "mes":8, "dia":22, 'name': 'Masacre de Trelew'},
+    {'user':0,"anio":1974, "hito":2,  "mes": 11, "dia":1, "name": "Inicio de actividades de ALUAR"},
+    {'user':0,"anio":1980, "hito":3,  "mes":2, 'dia':25, "name": "Fundación UNPSJB"},
+    {'user':0,"anio":1982, "hito":4,  "mes": 11, "dia":1, "name": "Guerra de Malvinas"},
+    {'user':0,"anio":1984, "hito":5,  'mes': 9, "dia":10, "name": "Madrynazo"},
+    {'user':0,"anio":1984, "hito":6,  'mes':12, "dia":14, "name": "Se creó la Sede Puerto Madryn de la UNPSJB"},
+    {'user':0,"anio":1994, "hito":7,  "mes": 11, "dia":1, "name": "Primer CENPAT abierto"},
+    {'user':0,"anio":2001, "hito":8,  "mes": 11, "dia":1, "name": "Crisis"},
+    {'user':0,"anio":2020, "hito":9, "mes":3,  'dia':1, "name": "Pandemia de COVID-19"},
+    
+    # {'user':0,"anio": 1991,'hito':1 ,"mes": 5, "dia":1, "name": "Inauguración edificio actual UNPSBJ sede Madryn"},
+    {'user':0,"anio": 2024,'hito':10, "mes": 11, "dia":1, "name": "Hoy - Festival"}
+]
+
+# Crear nombre de archivo con horario
+ahora = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+nombre_archivo = f"hitos_{ahora}.xlsx"
+ruta_archivo = "./"+nombre_archivo
+
+def guardar_hito(diccionario):
+    global ruta_archivo
+    nueva_fila = pd.DataFrame([diccionario])
+    if os.path.exists(ruta_archivo):
+        # Cargar archivo existente
+        df = pd.read_excel(ruta_archivo)
+        # Agregar nueva fila
+        df = pd.concat([df, nueva_fila], ignore_index=True)
+    else:
+        # Crear nuevo archivo con la primera fila
+        df = nueva_fila
+    
+    # Guardar DataFrame en el archivo Excel
+    df.to_excel(ruta_archivo, index=False)
+    
+
+data = []
+hitos_n = 12
+
+# Buscar archivos Excel en la carpeta que coincidan con el nombre base
+archivos_existentes = [
+    archivo for archivo in os.listdir(".")
+    if archivo.startswith('hitos_') and archivo.endswith(".xlsx")
+]
+
+if archivos_existentes:
+    print('LEVANTË UN ARCHIVO')
+    # Obtener el archivo más reciente basado en el nombre
+    archivos_existentes.sort()  # Asegura el orden cronológico por nombre
+    ruta_archivo = os.path.join('./', archivos_existentes[-1])
+
+    # Leer el archivo Excel
+    df = pd.read_excel(ruta_archivo)
+
+    df['hito'] = range(0, df.shape[0])
+    # Convertir filas a una lista de diccionarios
+    data = df.to_dict(orient="records")[-CANT_HITOS_USER:]
+    hitos_n = data[-1]['hito'] + 1
+
+cant_hitos= len(data)
+
+@app.route('/', methods=['GET'])
+def get_inicio():
+    # Devuelve los datos iniciales como JSON
+    return render_template('index.html')
+
+@app.route('/hito', methods=['GET'])
+def get_hito():
+    # Devuelve los datos iniciales como JSON
+    return render_template('agregar_hito.html')
+
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    # Devuelve los datos iniciales como JSON
+    return jsonify(data_initial+data)
+
+@socketio.on('connect')
+def handle_connect():
+    # Envía los datos iniciales al cliente cuando se conecta
+    emit('update_data', data_initial+data)
+
+@socketio.on('new_data')
+def handle_new_data(new_data):
+    # Agrega nuevos datos y emite el evento a todos los clientes conectados
+    global hitos_n, cant_hitos
+    new_data['hito'] = hitos_n
+    new_data['user'] = 1
+    hitos_n+=1
+    cant_hitos+=1
+    data.append(new_data)
+    guardar_hito(new_data)
+    if cant_hitos>CANT_HITOS_USER:
+        data.remove(data[0])
+    emit('update_data', data_initial+data, broadcast=True)
+    # data_sorted = sorted(data, key=lambda x: x["anio"])
+
+    # emit('update_data', data_sorted, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, 
+                 port='5000',
+                 debug=True)
---
+++ festival/static/index.js
@@ -0,0 +1,174 @@
+
+  //LABELLA JS
+  
+  // var data = [
+  //   {date: new Date(1970, 4,18), 
+  //     name:'Centro Nacional Patagónico (CENPAT) comenzó a funcionar'},
+  //   {date: new Date(1991, 4,18), 
+  //     name: 'Inauguración edificio actual UNPSBJ sede Madryn'},
+
+  //   {date: new Date(2024, 11,21), 
+  //     name: 'Hoy - Festival'},
+  // ];
+
+var data_save = null
+
+var init_timeline = function (data) {
+  var options =   {
+    margin: {left: 20, right: 20, top: 20, bottom: 20},
+    initialWidth: 1000,
+    initialHeight: 1000,
+  };
+  
+  var innerWidth =  options.initialWidth - options.margin.left - options.margin.right;
+  var innerHeight = options.initialHeight - options.margin.top - options.margin.bottom;
+  var colorScale = d3.scale.category10();
+  //var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+  d3.select('#timeline').html('');  
+  var vis = d3.select('#timeline')
+    .append('svg')
+    .attr('width',  options.initialWidth)
+    .attr('height', options.initialHeight)
+    .append('g')
+    .attr('transform', 'translate('+(options.margin.left)+','+(options.margin.top)+')');
+  
+  function labelText(d){
+    return d.anio + ' - ' + d.name;
+  }
+  
+  
+  
+  var dummyText = vis.append('text');
+  
+  var timeScale = d3.time.scale()
+    .domain(d3.extent(data, function(d){return d.date;}))
+    .range([0, innerHeight])
+    .nice();
+  
+  var nodes = data.map(function(movie){
+    var bbox = dummyText.text(labelText(movie))[0][0].getBBox();
+    movie.h = bbox.height;
+    movie.w = bbox.width;
+    return new labella.Node(timeScale(movie.date), movie.h + 4, movie);
+  });
+  
+  dummyText.remove();
+  
+  vis.append('line')
+    .classed('timeline', true)
+    .attr('y2', innerHeight);
+  
+  var linkLayer = vis.append('g');
+  var labelLayer = vis.append('g');
+  var dotLayer = vis.append('g');
+  
+  dotLayer.selectAll('circle.dot')
+    .data(nodes)
+    .enter().append('circle')
+    .classed('dot', true)
+    .attr('r', 3)
+    .attr('cy', function(d){return d.getRoot().idealPos;});
+  
+  function color(d,i){
+    //return '#1D1F20';
+    if (d.data.user == 0){
+      return '#1D1F20';
+    }else{
+      return '#96c93d';
+    }
+    
+  }
+  
+  var renderer = new labella.Renderer({
+    layerGap: 60,
+    nodeHeight: nodes[0].width,
+    direction: 'right'
+  });
+  
+  function draw(nodes){
+    renderer.layout(nodes);
+  
+    var sEnter = labelLayer.selectAll('rect.flag')
+      .data(nodes)
+      .enter().append('g')
+      .attr('transform', function(d){return 'translate('+(d.x)+','+(d.y-d.dy/2)+')';});
+  
+    sEnter
+      .append('rect')
+      .classed('flag', true)
+      .attr('width', function(d){ return d.data.w +  (d.data.w  * 0.5); })
+      .attr('height', function(d){ return d.dy; })
+      .attr('rx', 2)
+      .attr('ry', 2)
+      .style('fill', color);
+  
+    sEnter
+      .append('text')
+      .attr('x', 4)
+      .attr('y', 15)
+      .style('fill', '#D4F6F0')
+      .text(function(d){return labelText(d.data);});
+  
+    linkLayer.selectAll('path.link')
+      .data(nodes)
+      .enter().append('path')
+      .classed('link', true)
+      .attr('d', function(d){return renderer.generatePath(d);})
+      .style('stroke', color)
+      .style('stroke-width',2)
+      .style('opacity', 0.6)
+      .style('fill', 'none');
+  }
+  
+  var force = new labella.Force({
+    minPos: -10
+  })
+    .nodes(nodes)
+    .compute();
+  
+  draw(force.nodes());
+  
+}  
+
+const socket = io.connect('http://127.0.0.1:5000');
+
+// // Obtener datos iniciales
+// fetch('/data')
+//     .then(response => response.json())
+//     .then(data => {
+//       // Recorre los elementos del array y actualiza la fecha
+//       const updatedData = data.map(item => {
+//         // Asegúrate de que la propiedad 'date' exista antes de modificarla
+        
+//           item.date = new Date(item.anio, item.mes, item.dia);  // Cambia el valor de 'date' a un objeto Date
+        
+//         return item;  // Retorna el elemento actualizado
+//       })
+//       init_timeline(updatedData);}
+//     )
+//     // .then(data => init_timeline(data));
+
+// Recibir datos nuevos a través del WebSocket
+   socket.on('update_data', function(data) {
+    const updatedData = data.map(item => {
+              item.date = new Date(item.anio, item.mes, item.dia);  // Cambia el valor de 'date' a un objeto Date
+              return item;})
+    if (data_save != null){
+      data.sort((a, b) => b.hito - a.hito);
+      Toastify({
+        newWindow: true,
+        close: true,
+        oldestFirst:false,
+        duration: 300000,
+        text: data[0].anio + ' - ' + data[0].name + ': ' + data[0].des,
+        className: "info",
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+      }).showToast();
+    }
+    data_save = updatedData
+    init_timeline(updatedData)
+});
+
